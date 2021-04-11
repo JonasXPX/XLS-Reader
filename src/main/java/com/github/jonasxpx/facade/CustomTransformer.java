b9867785660fb0c36da962cd8f9b8d5c3e175e79
Jonas Peretiatko <jonas.peretiatko@desbravador.com.br>
2021-04-11 12:07:08 -0300

    no refs. updated example transformer

--- src/main/java/com/github/jonasxpx/facade/CustomTransformer.java
+++ src/main/java/com/github/jonasxpx/facade/CustomTransformer.java
@@ -1,7 +1,7 @@
 package com.github.jonasxpx.facade;
 
 import com.github.jonasxpx.anotations.TransformerRunner;
-
+//example class
 public class CustomTransformer implements TransformerRunner<String> {
     @Override
     public String run(String entity) {
